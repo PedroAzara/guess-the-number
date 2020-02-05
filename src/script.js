f36c98a9cfd574c947cc06df58af2596fb708ee7
Pedro Henrique <pedrohenriquealmeida2004@gmail.com>
2020-02-04 21:18:50 -0300

    corrigido bugs/ interface semipronta

--- src/script.js
+++ src/script.js
@@ -28,7 +28,8 @@ function notificacaoAcerto(){
     divMsg = document.getElementById('msg1')
     divMsg.innerHTML = 'Parabéns!<br>Você <strong>acertou!</strong>'
     
-    setTimeout(desaparecer, 3500)
+    
+
 }
 
 function notificacaoErro(){
@@ -38,28 +39,34 @@ function notificacaoErro(){
     divMsg = document.getElementById('msg1')
     divMsg.innerHTML = 'Desculpe<br>Você errou, tente novamente :('
     
-    setTimeout(desaparecer, 3500)
+    
+    
 }
 
 var desaparecer = function desaparecerNotificacao(){
   divCaixinha.classList.remove('is-success')
   divCaixinha.classList.remove('is-danger')
   divMsg.innerHTML = 'Digite um número de 1 a 100 e clique em "Confirmar!" para ver seu resultado!'
+  numeroQERA = document.getElementById('numeroEra')
+  numeroQERA.innerHTML = `Resultado`
 }
 
 function escolherNumero(){
   function gerarNumeroAleatorio(){
     return Math.floor(Math.random() * 100)}
+  var numeroRandom = gerarNumeroAleatorio()
   numeroJogadorFormatado = Number.parseInt(document.getElementById('numeroJogador').value)
   if(numeroJogadorFormatado >= 0 && numeroJogadorFormatado <=100){
     console.log(numeroJogadorFormatado)
     document.getElementById('aviso').classList.add('invisible')
-    if(numeroJogadorFormatado === 1){
-      notificacaoAcerto()
+    numeroQERA = document.getElementById('numeroEra')
+    numeroQERA.innerHTML = `O número era ${numeroRandom}`
+    if(numeroJogadorFormatado === numeroRandom){
+      notificacaoAcerto()   
     }else{
       notificacaoErro()
-      
     }
+    setTimeout(desaparecer, 8000)
     
   }else{
     document.getElementById('aviso').classList.remove('invisible')
